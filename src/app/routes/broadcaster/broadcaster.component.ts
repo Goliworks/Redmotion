7b7fc51efc62f29e5e348837314af7c0c171a9dd
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-14 22:50:19 +0200

    feat: broadcaster route

---
+++ src/app/routes/broadcaster/broadcaster.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-broadcaster',
+  standalone: true,
+  imports: [],
+  templateUrl: './broadcaster.component.html',
+  styleUrl: './broadcaster.component.scss'
+})
+export class BroadcasterComponent {
+
+}
