a029818a8cc041ae9d63fec0a2583657bfb04216
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-19 18:11:24 +0200

    feat: viewport component

--- src/app/routes/broadcaster/broadcaster.component.ts
+++ src/app/routes/broadcaster/broadcaster.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SplitterModule } from 'primeng/splitter';
+import { ViewportComponent } from '../../components/viewport/viewport.component';
 
 @Component({
   selector: 'app-broadcaster',
   standalone: true,
-  imports: [SplitterModule],
+  imports: [SplitterModule, ViewportComponent],
   templateUrl: './broadcaster.component.html',
   styleUrl: './broadcaster.component.scss',
 })
