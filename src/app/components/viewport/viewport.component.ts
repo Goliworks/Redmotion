816b83130c980ff70375faf4bc06b31211ff1383
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-19 22:48:13 +0200

    feat: init pixi viewport

--- src/app/components/viewport/viewport.component.ts
+++ src/app/components/viewport/viewport.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Application } from 'pixi.js';
 
 @Component({
   selector: 'app-viewport',
   standalone: true,
   imports: [],
   templateUrl: './viewport.component.html',
-  styleUrl: './viewport.component.scss'
+  styleUrl: './viewport.component.scss',
 })
-export class ViewportComponent {
+export class ViewportComponent implements OnInit {
+  @ViewChild('viewport') viewport!: ElementRef;
 
+  async ngOnInit() {
+    const app = new Application();
+    await app.init();
+
+    // Resize canvas content when viewport is resized.
+    const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+      app.resizeTo = entries[0].target as HTMLElement;
+    });
+
+    observer.observe(this.viewport.nativeElement);
+
+    this.viewport.nativeElement.appendChild(app.canvas);
+  }
 }
