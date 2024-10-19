fc94f4624af023027ed1a581c73cf3d8aa211a2b
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-20 00:06:49 +0200

    feat: draw working area

--- src/app/components/viewport/viewport.component.ts
+++ src/app/components/viewport/viewport.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Application } from 'pixi.js';
+import { Application, Container, Graphics } from 'pixi.js';
 
 @Component({
   selector: 'app-viewport',
@@ -11,9 +11,12 @@ import { Application } from 'pixi.js';
 export class ViewportComponent implements OnInit {
   @ViewChild('viewport') viewport!: ElementRef;
 
+  private projectWidth = 1920;
+  private projectHeight = 1080;
+
   async ngOnInit() {
     const app = new Application();
-    await app.init();
+    await app.init({ background: '#222426' });
 
     // Resize canvas content when viewport is resized.
     const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
@@ -23,5 +26,19 @@ export class ViewportComponent implements OnInit {
     observer.observe(this.viewport.nativeElement);
 
     this.viewport.nativeElement.appendChild(app.canvas);
+
+    const workingArea = new Container();
+    app.stage.addChild(workingArea);
+
+    app.ticker.add(() => {
+      workingArea.x = app.renderer.width / 2 - this.projectWidth / 2;
+      workingArea.y = app.renderer.height / 2 - this.projectHeight / 2;
+    });
+
+    const visibleZone = new Graphics();
+    visibleZone.rect(0, 0, this.projectWidth, this.projectHeight);
+    visibleZone.fill(0x3b3f42);
+
+    workingArea.addChild(visibleZone);
   }
 }
