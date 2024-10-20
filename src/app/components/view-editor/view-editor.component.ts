7641aa421de8e7bde94659a8c87163153fd1c2db
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-20 02:02:09 +0200

    feat: view editor component

---
+++ src/app/components/view-editor/view-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-editor.component.html',
+  styleUrl: './view-editor.component.scss'
+})
+export class ViewEditorComponent {
+
+}
