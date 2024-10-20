ba5f1e425af9dca980dcdf09f4cec458e5d7271f
Guillaume (Goliver) Golay <goliworks@gmail.com>
2024-10-20 02:30:02 +0200

    feat: compose view editor

--- src/app/components/view-editor/view-editor.component.ts
+++ src/app/components/view-editor/view-editor.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ViewportComponent } from '../viewport/viewport.component';
+import { ViewportBarComponent } from '../viewport-bar/viewport-bar.component';
 
 @Component({
   selector: 'app-view-editor',
   standalone: true,
-  imports: [],
+  imports: [ViewportComponent, ViewportBarComponent],
   templateUrl: './view-editor.component.html',
-  styleUrl: './view-editor.component.scss'
+  styleUrl: './view-editor.component.scss',
 })
-export class ViewEditorComponent {
-
-}
+export class ViewEditorComponent {}
--- src/app/routes/broadcaster/broadcaster.component.ts
+++ src/app/routes/broadcaster/broadcaster.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SplitterModule } from 'primeng/splitter';
 import { ViewportComponent } from '../../components/viewport/viewport.component';
+import { ViewEditorComponent } from '../../components/view-editor/view-editor.component';
 
 @Component({
   selector: 'app-broadcaster',
   standalone: true,
-  imports: [SplitterModule, ViewportComponent],
+  imports: [SplitterModule, ViewEditorComponent],
   templateUrl: './broadcaster.component.html',
   styleUrl: './broadcaster.component.scss',
 })
